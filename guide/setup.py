7914f67d9356ac14536e19c887d268c92bc4a339
andras-makany <andrasmakany@gmail.com>
2026-07-14 12:35:07 +0200

    build: switch remaining packages to find_namespace_packages

--- guide/setup.py
+++ guide/setup.py
@@ -1,11 +1,11 @@
-from setuptools import find_packages, setup
+from setuptools import find_namespace_packages, setup
 
 package_name = "guide"
 
 setup(
     name=package_name,
     version="1.0.0",
-    packages=find_packages(exclude=["test"]),
+    packages=find_namespace_packages(include=[package_name, f"{package_name}.*"]),
     data_files=[
         ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
         ("share/" + package_name, ["package.xml"]),
--- guide_ex/setup.py
+++ guide_ex/setup.py
@@ -1,14 +1,14 @@
 import os
 from glob import glob
 
-from setuptools import find_packages, setup
+from setuptools import find_namespace_packages, setup
 
 package_name = "guide_ex"
 
 setup(
     name=package_name,
     version="0.1.0",
-    packages=find_packages(exclude=["test"]),
+    packages=find_namespace_packages(include=[package_name, f"{package_name}.*"]),
     data_files=[
         ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
         ("share/" + package_name, ["package.xml"]),
